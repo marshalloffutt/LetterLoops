926f6bc54a0cf10889b2a29f724f40ef0c6abf2f
Marshall Offutt <marshalloffutt@gmail.com>
2019-02-28 17:37:57 -0600

    Finish program

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -6,7 +6,32 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type some letters");
+            string input = Console.ReadLine();
+            string output = "";
+
+            // Iterate over the input string
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Write a capital letter to ouput at each indexed letter
+                output += char.ToUpper(input[i]);
+
+                // Iterate over each character the number of times equal to the index
+                for (int j = 0; j < i; j++)
+                {
+                    // At each iteration, write a lowercase letter
+                    output += char.ToLower(input[i]);
+                }
+
+                // Add a dash for every indexed character
+                output += "-";
+            }
+
+            // Remove the last dash
+            string finalOutput = output.Remove(output.Length - 1);
+
+            Console.WriteLine(finalOutput);
+            Console.ReadKey();
         }
     }
 }
